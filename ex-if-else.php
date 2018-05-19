eb9ef7d6ffee8a9b3c1a6ff6f4369083ab216b36
Marcus Neri <marcusneri_joga10@hotmail.com>
2018-05-19 17:25:39 -0300

    if else

---
+++ ex-if-else.php
@@ -0,0 +1,21 @@
+<?php
+
+//CLI - Command line Interface
+// Interface de linha de comando
+
+$idade = readline('Digite sua idade: ');
+ 
+ 
+  if ($idade >= 18) {
+   echo 'Vocé é maior de idade' . PHP_EOL;
+
+ } else {
+
+ 	echo 'Voce é menor de idade' . PHP_EOL;
+
+
+
+
+ }
+
+
---
+++ if.php
@@ -0,0 +1,23 @@
+<?php
+
+$condicao = false;
+
+
+if ($condicao) {
+
+   echo 'verdadeiro' ;
+
+
+}
+
+$idade = 20;
+
+if($idade >= 15) {
+
+echo 'ele tem ' . $idade . ' portanto é Maior de idade';
+
+
+} else {
+echo  'ele tem ' . $idade .' portanto é menor de idade';
+
+}
